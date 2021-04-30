89ea55831e5af461e5f4019530844467c4f811fe
verongable <77641365+verongable@users.noreply.github.com>
2021-04-30 12:24:54 -0700

    Update index.php

--- big/index.php
+++ big/index.php
@@ -1,8 +1,6 @@
-<?php include 'includes/header.php';?>
-
+<?php include "../includes/header.php";?>
 <section>
     <h2 class="pageID">BIG</h2>
     <p>Use this as a model for creating additional pages.</p>
 <section>
-
-<?php include 'includes/footer.php';?>
+<?php include "../includes/footer.php";?>
