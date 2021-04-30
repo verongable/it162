520c007d21cd7b06d1ae866e3cda4ae69b94b405
verongable <77641365+verongable@users.noreply.github.com>
2021-04-29 19:30:28 -0600

    BIG

---
+++ big/index.php
@@ -0,0 +1,8 @@
+<?php include 'includes/header.php';?>
+
+<section>
+    <h2 class="pageID">BIG</h2>
+    <p>Use this as a model for creating additional pages.</p>
+<section>
+
+<?php include 'includes/footer.php';?>
