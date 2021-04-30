c7bdaa83b971b74f9d3da548d1ff41c3b4decd79
verongable <77641365+verongable@users.noreply.github.com>
2021-04-29 19:37:42 -0600

    Flowchart

---
+++ flowchart.php
@@ -0,0 +1,6 @@
+<?php include 'includes/header.php';?>
+
+<section>
+    <h2 class="pageID">Flowchart</h2>
+    <p>Use this as a model for creating additional pages.</p>
+<?php include 'includes/footer.php';?>
