829d53df290912ff247fbbfc1e3ef80e003c91ff
verongable <77641365+verongable@users.noreply.github.com>
2021-04-29 18:27:56 -0700

    Delete template.php

--- template.php
+++
@@ -1,7 +0,0 @@
-<?php include 'includes/header.php';?>
-
-<section>
-    <h2 class="pageID">Template</h2>
-    <p>Use this as a model for creating additional pages.</p>
-</section>
-<?php include 'includes/footer.php';?>
