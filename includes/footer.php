339b7b80fe78630785c85d2e0b81bf43fc54e5c1
verongable <77641365+verongable@users.noreply.github.com>
2021-04-29 19:24:02 -0600

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,21 @@
+<!-- START Footer -->
+<footer>
+    <p><small>&copy; 2018 - <?=date('Y')?> by <a href="contactme.php" target="_blank">Veronica Gable</a>, All Rights Reserved - <a href="https://validator.w3.org/check/referer" target="_blank">Valid HTML</a> - <a href="http://jigsaw.w3.org/css-validator/validator?uri=https%3A%2F%2Fwww.veronicagable.com%2Fit162%2F&profile=css3svg&usermedium=all&warning=1&vextwarning=" target="_blank">Valid CSS</a></small></p>
+  </footer>
+</main>
+<!-- END WRAPPER -->
+
+    <!-- JavaScript associated with the W3Schools.com Top Navigation Response Exercise --> 
+    <script>
+        /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+        function myFunction() {
+            var x = document.getElementById("myTopnav");
+            if (x.className === "topnav") {
+                x.className += " responsive";
+            } else {
+                x.className = "topnav";
+            }
+        }
+    </script>        
+</body>
+</html>
