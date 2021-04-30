cd9e7bea80f46d89e0b97d08272e1ba00e02b475
verongable <77641365+verongable@users.noreply.github.com>
2021-04-29 19:16:30 -0600

    Add files via upload

---
+++ template.php
@@ -0,0 +1,7 @@
+<?php include 'includes/header.php';?>
+
+<section>
+    <h2 class="pageID">Template</h2>
+    <p>Use this as a model for creating additional pages.</p>
+</section>
+<?php include 'includes/footer.php';?>
