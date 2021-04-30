7a25b446e03ab1bca8210fdee1379e82f4f8ae43
verongable <77641365+verongable@users.noreply.github.com>
2021-04-29 19:29:28 -0600

    Portal Homepage

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php include 'includes/header.php';?>
+<!-- START LEFT COL -->
+<section>
+    <h2 class="pageID">Welcome To My Website!</h2>
+   
+   <!-- MAKE SURE YOU GET YOUR (3) IMAGES SAVED INTO YOUR IMAGES FOLDER -->
+    <img src="images/desktop.jpeg" class="desktop" alt="Picture of Myself" />
+    <img src="images/phone.jpeg" class="phone" alt="At a Japanese Flower Garden" />
+    <p>Hello everyone! My name is Veronica Gable. Thank you all for taking the time to explore my IT162 website.</p>
+    <p>I've always been drawn to the front end aspect of web development. I believe that in order to have a great product, it should not only be visually appealing, but it needs to also<em> work </em>seemlesy and effectively.</p>
+    <p>During my time in the Web Design program at SCC, I hope to learn all the essential skills in order to develop properly working websites.</p>
+</section>
+   <!-- END LEFT COL -->
+   <!-- START RIGHT COL -->
+<aside>
+    <h3>When I'm not busy doing homework, I like to spend my free time:</h3>
+    <img src="images/tablet.jpeg" class="tablet" alt="Eating Food" />
+    <ul>
+        <li>Playing with my dogs Igor & Charlie</li>
+        <li>Traveling with my husband, Shane</li>
+        <li>Going out to brunch</li>
+        <li>Listening to podcasts while I clean</li>
+        <li>Doing pilates</li>
+        <li>Drinking coffee</li>
+
+    </ul>
+   </aside>
+   <!-- END RIGHT COL -->
+
+<?php include 'includes/footer.php';?>
