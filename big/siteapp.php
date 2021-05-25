4f380cd6ccc7f24345694cb3bc5a04c861016c4b
Veronica G <77641365+verongable@users.noreply.github.com>
2021-05-24 17:30:11 -0700

    final siteapp.php

--- big/siteapp.php
+++ big/siteapp.php
@@ -1,23 +1,42 @@
 <?php include "includes/header.php"?>
     <p>The prevalence of the internet has grown immensely over the years. During this time, the rise of smartphone users has made it evident that both website developers and businesses need to optimize their products for mobile use. In order to implement this, businesses need to decide whether to build a responsive website or a mobile app. In order to understand which process is better we need to learn the distinctions between the two as well as their pros and cons.</p>
 
+    <img src="images/siteapp-image-1.jpeg" alt="Zillow Website and App">
+
     <p>A responsive website, which is sometimes known as a ‘web app’, refers to a website that’s designed with a dynamic interface and is typically built using the “mobile-first” method. This kind of design uses both fluid grid systems and flexible images, as well as media queries that change the layout depending on set parameters. All these qualities ensure the website design can adapt both to mobile devices and larger screens. These websites are accessed through a browser. On the other hand, a mobile app, also known as a native app, is made specifically for smartphones and needs to be downloaded through Application stores. Mobile apps are designed differently depending on the mobile operating system they will run on. </p>
 
+    <img src="images/siteapp-image-2.png" alt="Mobile First Approach">
+
+    <h2>Advantages of Responsive Websites</h2>
     <p>The advantages of building a responsive website are noticeable in that they require less resources. Since the website is being displayed in a browser, less CPU and storage space is required than a mobile app. Responsive websites also take less time to market since you don’t need to go through any of the approval steps that mobile apps require. If you need to change your design, a simple code update is sufficient. The cost of production will also be significantly lower because you don’t have to worry about adjusting your designs for different platforms.</p>
 
+    <h2>Disadvantages of Responsive Websites</h2>
     <p>The downsides of designing a responsive website are that there is a limited support of native device features such as the gyro, accelerometer, and push notifications. Responsive websites also require constant internet connectivity in order to be able to make use of the website, unlike mobile apps which can be created to support offline use.</p>
 
-    <p>Moving on, mobile apps have their own advantages as well. They have the capacity to store crucial data directly in the device instead of relying on a web browser in order to execute necessary functions, meaning that they are able to perform a lot faster than responsive websites. Mobile apps are also ideal in that you can accomplish more in terms of User Experience (UX) since you are not limited to a web browser, meaning you have the ability to manipulate the interface until it meets your needs. Another benefit of creating a mobile app is that you can monetize your visitor traffic via in-app purchases instead of advertising. This process is made easier through the commerce services that app stores normally provide.</p>
+    <h2>Advantages of Mobile Apps</h2>
+    <p>Moving on, mobile apps have their own advantages too. They have the capacity to store crucial data directly in the device instead of relying on a web browser in order to execute necessary functions, meaning that they are able to perform a lot faster than responsive websites. Mobile apps are also ideal in that you can accomplish more in terms of User Experience (UX) since you are not limited to a web browser, meaning you have the ability to manipulate the interface until it meets your needs. Another benefit of creating a mobile app is that you can monetize your visitor traffic via in-app purchases instead of advertising. This process is made easier through the commerce services that app stores normally provide.</p>
 
+    <img src="images/siteapp-image-3.png" alt="Mobile Apps">
+
+    <h2>Disadvantages of Mobile Apps</h2>
     <p>The disadvantages of mobile apps are that it’s usually harder to obtain new users. Simply having your app available on an app store won’t guarantee users will be motivated to install your app. According to Adobe, “Even when the user installs your app, 21% of users abandon an app after one use. (Nick Babich)” With this in mind, businesses will have to invest notable resources in marketing and advertising in order to increase downloads. A final disadvantage of web apps is that since they’re able to run faster, they inevitably result in slowing down processes that are running in the background.</p>
 
+    <h2>Suit Your Needs</h2>
     <p>Despite having their upsides and downsides, both responsive websites and mobile apps are equal contenders. After going through the pros and cons of each, the decision should be dependent on what your needs are. Think about your goals, budget, time, and audience to find the best option(s) for you.</p>
 </section>
+</div>
    <!-- END LEFT COL -->
-   <!-- START RIGHT COL --> 
+   <!-- START RIGHT COL -->
 <aside>
+<div class="research-aside">
     <h3>Sources:</h3>
-    <p>Place sources here.</p>
+      <h4>Adobe</h4>
+        <p>Adobe. “Deciding Between Responsive Web or Native Apps | Adobe XD.” Adobe, Nick Babich, 15 Apr. 2020, xd.adobe.com/ideas/principles/web-design/responsive-web-or-native-app/#:%7E:text=It%20lowers%20the%20cost%20of,good%20on%20any%20screen%20size.</p>
+      <h4>INVID</h4>
+        <p>Feliciano, Carlos. “Native vs. Responsive Apps: Which One You Should Choose?” INVID, 21 Oct. 2020, invidgroup.com/native-vs-responsive-apps-which-to-choose/.</p>
+      <h4>Starmark</h4>
+        <p>Starmark. “Native Mobile App or Responsive Web Design?” Starmark | Integrated Marketing Communications, 17 June 2016, www.starmark.com/blog/etips/native-mobile-app-or-responsive-web-design/.</p>
    </aside>
+   </main>
    <!-- END RIGHT COL -->
    <?php include "includes/footer.php"?>
