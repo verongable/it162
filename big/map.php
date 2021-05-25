fb7fc754068fbe941d74702e9546162e37a5624b
Veronica G <77641365+verongable@users.noreply.github.com>
2021-05-24 17:28:25 -0700

    final map.php

--- big/map.php
+++ big/map.php
@@ -1,13 +1,13 @@
 <?php include "includes/header.php"?>
-<section>
-    <div class="embed-container"><iframe src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2689.5050995528613!2d-122.32339218436917!3d47.616312079185334!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x54906accc351c149%3A0xdc1a5c338dd4395c!2sSeattle%20Central%20College!5e0!3m2!1sen!2sus!4v1613610814984!5m2!1sen!2sus' width='600' height='450' aria-hidden='false' tabindex='0'></iframe></div>
-</section>
-   <!-- END LEFT COL -->
-   <!-- START RIGHT COL --> 
-<aside>
-    <h3>Seattle Central College</h3>
-    <address>1701 Broadway, Seattle, WA 98122</address>
-    <a href="tel:012069343800">(206) 934-3800</a>
-   </aside>
-   <!-- END RIGHT COL -->
-   <?php include "includes/footer.php"?>
+	<div class="embed-container">
+		<iframe src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2689.5050995528613!2d-122.32339218436917!3d47.616312079185334!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x54906accc351c149%3A0xdc1a5c338dd4395c!2sSeattle%20Central%20College!5e0!3m2!1sen!2sus!4v1613610814984!5m2!1sen!2sus' width='600' height='450' aria-hidden='false' tabindex='0'></iframe>
+	</div>
+	</section>
+	<!-- END LEFT COL -->
+	<!-- START RIGHT COL -->
+	<div class="google-aside">
+		<h2>Seattle Central College</h2>
+		<h3><address>1701 Broadway, Seattle, WA 98122</address></h3> <a href="tel:012069343800">(206) 934-3800</a> </div>
+	</main>
+	<!-- END RIGHT COL -->
+	<?php include "includes/footer.php"?>
