4439885a8249c7fa3a448f585bd46df947c7c72e
Veronica G <77641365+verongable@users.noreply.github.com>
2021-05-24 17:29:40 -0700

    final shoppingcarts.php

--- big/shoppingcarts.php
+++ big/shoppingcarts.php
@@ -1,17 +1,55 @@
 <?php include "includes/header.php"?>
-    <div class='embed-container'><iframe src='https://www.youtube.com/embed/hufMi9LZX2I' frameborder='0' allowfullscreen></iframe></div>
-    <p class="video-caption">This quick video talks about the importance of web accessibility.</p>
+<img src="images/shoppingcart-image-1.png" alt="eCommerce">
+    <p>An online shopping cart is a type of software that contains unique features that allow you and your customers to make purchases from your website. In order to start selling items/services on your website, you need to have a shopping cart that can process payments. There are many different types of shopping cart software available. Some of them are simple and easy to use, and provide the basics while others have more up to the minute features. Whether you are new to e-commerce or a seasoned pro, you will surely find a shopping cart that suits your needs.</p>
+
+    <p>An online shopping cart is very similar to an actual store in that there is a customer-facing side and an employee/business-facing side. The customer side is commonly known as the “frontend” or the “storefront” and is represented by the website that customers browse. The employee/business-facing side would be the “backend” and encompasses the administrative side. Both work hand in hand, allowing your customers to place orders, and allowing you to process those orders.</p>
+
+    <img src="images/shoppingcart-image-2.jpeg" alt="online shopping">
+
+    <p>If you have a small business and are interested in adding a shopping cart to an existing website, I would suggest using the hosted shopping cart software (Software-as-a-Service (SaaS)) method, which is a versatile platform that lets you launch your own online store. Unlike other shopping cart software, download or installation is not required. The software is delivered as a cloud-based system, which is then accessed via web browser. This type of service usually requires a monthly or annual subscription plan. Although SaaS platforms are popular for being fast and effective, it might not be ideal for someone who doesn’t want to commit to a subscription service and is only interested in selling a couple of items. In this case, I suggest using <a href="https://www.paypal.com/us/webapps/mpp/home" target="_blank"><strong>PayPal’s</strong></a> shopping cart feature.</p>
+
+    <p>If you are not familiar with PayPal, they are a payment provider that allows you to accept debit, credit cards, PayPal, and local payments on purchases made by your customers. Currently, they have a free HTML shopping cart feature available for users to use on their websites. It is the quickest and simplest method to try if you are just getting started.</p>
+
+
+    <p>Using PayPals Payment Buttons on your website is a great alternative for those who have a small inventory. Every time a transaction is completed, the funds go straight into your PayPal account. PayPal lets you keep track of inventory, they have an easy-to-follow refund process; they have a thank you/cancellation page feature, and you can customize your purchase screen as well.</p>
+
+    <h2>How to Sell Items with a PayPal Shopping Cart</h2>
+
+    <img src="images/shoppingcart-image-3.jpeg" alt="PayPal">
+
+        <h3>1. Sign up for a Business Account</h3>
+            <p>In order to start selling items/services to your customers, and start processing online payments, you need to have a <a href="https://www.paypal.com/business" target="_blank"><strong>PayPal Business</strong></a> account. If you already have a personal account, you can upgrade your account by going to the Settings in the top right corner. Under Account Options, choose Upgrade to a Business Account.</p>
+
+            <p>After going through the sign-up process, it will direct you to your business account Dashboard.</p>
+        <h3>2. Create Payment Buttons</h3>
+            <p>Paypal has several <a href="https://www.paypal.com/buttons/" target="_blank"><strong>customizable button</strong></a> options. Depending on your needs, you can choose between Buy Now, Add to Cart, Donation, Subscriptions, and Automatic Billing.</p>
+            <p>To get started, go to PayPal’s Payment Button page, choose the button you would like to use, once you have finished going through the customization steps, select Create Button.</p>
+        <h3>3. Add Button Code to Website</h3>
+            <p>Once you’ve been directed to your generated PayPal payment button code, copy the HTML, go to your website, and paste your code where you would like your button to be. Once you save and upload your files, your PayPal shopping cart buttons should be fully functional. If you would like, you can further customize your button by modifying your HTML and CSS code.</p>
+
+    <p>A hosted shopping cart provides all the features needed to fulfill the e-commerce experience, but it takes more time to set up and manage, and typically does require a fee. If what you intend to sell doesn’t offset the costs, PayPal Payment Buttons might be a better option for you.</p>
 </section>
    <!-- END LEFT COL -->
    <!-- START RIGHT COL --> 
-   <div class="google-aside">
-    <h2>UX/UI on YouTube:</h2>
-<ul>
-   <li><a href="https://www.youtube.com/watch?v=HL1-Be71URU" target="_blank">UX/UI Design Process</a></li>
-   <li><a href="https://www.youtube.com/watch?v=ZZ1xffF7P78" target="_blank">Interview Process</a></li>
-   <li><a href="https://www.youtube.com/watch?v=MLaXv1e4wHQ" target="_blank">The Theory Behind Color</a></li>
-</ul>
-    </div>
+<aside>
+<div class="research-aside">
+    <h3>Sources:</h3>
+        <h4>3D Cart</h4>
+            <p>Cannon, Christianna. “How to Add a Shopping Cart to Your Website.” 3dcart, 19 Jan. 2021, blog.shift4shop.com/how-to-add-a-shopping-cart-to-your-website.
+            </p>
+        <h4>Build-Your-Website</h4>
+            <p>Cope, Steve. “How to Add a Simple PayPal HTML Shopping Cart to Your Website.” Build Your Website |, 21 Nov. 2017, www.build-your-website.co.uk/how-to-add-shopping-cart-website.“
+            </p>
+        <h4>Lifewire</h4>
+            <p>Create a Simple PayPal Shopping Cart.” Lifewire, www.lifewire.com/create-shopping-cart-with-paypal-3467263. Accessed 21 May 2021.
+            </p>
+        <h4>Charleston PR & Design</h4>
+            <p>Smithem, Cheryl. “Pros and Cons of Using a PayPal Button.” Charleston PR & Design, LLC, 21 June 2016, charlestonpr.com/pros-cons-paypal-button.
+            </p>
+        <h4>Sana Commerce</h4>
+            <p>“What Is SaaS E-Commerce? | E-Commerce Terms.” Sana Commerce, 5 May 2021, www.sana-commerce.com/e-commerce-terms/what-is-saas-e-commerce.
+            </p>
+   </aside>
    </main>
    <!-- END RIGHT COL -->
    <?php include "includes/footer.php"?>
