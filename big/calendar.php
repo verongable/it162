707874e38d337d16d3d91fa07e1c6f50c16429ac
Veronica G <77641365+verongable@users.noreply.github.com>
2021-05-24 17:26:55 -0700

    final calendar.php

--- big/calendar.php
+++ big/calendar.php
@@ -1,11 +1,22 @@
 <?php include "includes/header.php"?>
-    <p>Place a responsive Google Calendar iframe here. It should have at least one event with link to event website.</p>
-</section>
-   <!-- END LEFT COL -->
-   <!-- START RIGHT COL --> 
-<aside>
-    <h3>Event Information</h3>
-    <p>Add web address and event info here.</p>
-   </aside>
-   <!-- END RIGHT COL -->
-   <?php include "includes/footer.php"?>
+	<!-- Show/Hide MONTH View Calendar based upon resolution -->
+	<div class='month-view embed-container'>
+		<iframe src="https://calendar.google.com/calendar/embed?height=600&amp;wkst=1&amp;bgcolor=%23B39DDB&amp;ctz=America%2FLos_Angeles&amp;src=bmJxcHBpOHZsYjFnNW1ia2NocjY3Y2QwdTBAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&amp;color=%23B39DDB&amp;showTitle=0" style="border:solid 1px #777" width="800" height="450" frameborder="0" scrolling="no"></iframe>
+	</div>
+	<!-- Show/Hide AGENDA View Calendar based upon resolution -->
+	<div class='agenda-view embed-container'>
+		<iframe src="https://calendar.google.com/calendar/embed?height=600&amp;wkst=1&amp;bgcolor=%23ffffff&amp;ctz=America%2FLos_Angeles&amp;src=bmJxcHBpOHZsYjFnNW1ia2NocjY3Y2QwdTBAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&amp;color=%23B39DDB&amp;showTitle=0&amp;mode=AGENDA" style="border:solid 1px #777" width="800" height="600" frameborder="0" scrolling="no"></iframe>
+	</div>
+	</section>
+	<!-- END LEFT COL -->
+	<!-- START RIGHT COL -->
+	<div class="google-aside">
+		<h2>Events:</h2>
+		<h3><a href="https://www.mopop.org/exhibitions-plus-events/exhibitions/body-of-work/" target="_blank">Body of Work: Tattoo Culture</a></h3>
+		<h4>Now - 05/31</h4>
+		<p>MoMA</p>
+		<p>325 5th Ave N, Seattle, WA 98109</p>
+		</aside>
+		</main>
+		<!-- END RIGHT COL -->
+		<?php include "includes/footer.php"?>
