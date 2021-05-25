d0c03993ced749fc6904e5499cf8191805e4acdd
Veronica G <77641365+verongable@users.noreply.github.com>
2021-05-24 17:31:06 -0700

    final youtube.php

--- big/youtube.php
+++ big/youtube.php
@@ -1,11 +1,17 @@
 <?php include "includes/header.php"?>
     <div class='embed-container'><iframe src='https://www.youtube.com/embed/hufMi9LZX2I' frameborder='0' allowfullscreen></iframe></div>
+    <p class="video-caption">This quick video talks about the importance of web accessibility.</p>
 </section>
    <!-- END LEFT COL -->
    <!-- START RIGHT COL --> 
-<aside>
-    <h3>Video Info:</h3>
-    <p>Explain video.</p>
-   </aside>
+   <div class="google-aside">
+    <h2>UX/UI on YouTube:</h2>
+<ul>
+   <li><a href="https://www.youtube.com/watch?v=HL1-Be71URU" target="_blank">UX/UI Design Process</a></li>
+   <li><a href="https://www.youtube.com/watch?v=ZZ1xffF7P78" target="_blank">Interview Process</a></li>
+   <li><a href="https://www.youtube.com/watch?v=MLaXv1e4wHQ" target="_blank">The Theory Behind Color</a></li>
+</ul>
+    </div>
+   </main>
    <!-- END RIGHT COL -->
    <?php include "includes/footer.php"?>
